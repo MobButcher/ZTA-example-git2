4c5bbffe8ced8c9b4a48c7287f3181f098cd4df0
Competition Organizers <38690257+MobButcher@users.noreply.github.com>
2022-02-04 17:31:09 +0200

    Create boilerplate for 2-nd competition problem "Implement a search algorithm on a sorted array of integers"

---
+++ competition/problem-2-search.js
@@ -0,0 +1,19 @@
+/** **Problem 2**: Implement a search algorithm on a sorted array of integers
+ * 
+ * > **Constraints:** 
+ * > - `1 <= arr.length <= 10_000_000;`
+ * > - All values in `arr` can be safely represented by Number: \
+ * >   `arr.all(Number.isInteger) === true;` \
+ * >   `arr.all(Number.isSafeInteger) === true;`
+ * 
+ * > **Restrictions:**
+ * > - Foreign libraries and files are disallowed.
+ * > - Using `Array.prototype` methods is disallowed.
+ * @param arr {number[]} Array to be searched through
+ * @param target {number} Value to be found in `arr`
+ * @returns {number} Index of `target` in an array, or -1 if no `target` found
+ */
+ function search(arr, target) {
+    // Your code goes here.
+    return arr[0];
+}
