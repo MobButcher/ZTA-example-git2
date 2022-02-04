72398100585c1fdff99b69eabff01edc5364b627
Competition Organizers <38690257+MobButcher@users.noreply.github.com>
2022-02-04 17:28:31 +0200

    Create boilerplate for 1-st competition problem "Implement a sorting alforithm for integers"

---
+++ competition/problem-1-sort.js
@@ -0,0 +1,18 @@
+/** **Problem 1**: Implement a sorting algorithm for integers
+ * 
+ * > **Constraints:**
+ * > - `1 <= input.length <= 10_000_000;`
+ * > - All values in `input` can be safely represented by `Number`: \
+ * >   `input.all(Number.isInteger) === true;` \
+ * >   `input.all(Number.isSafeInteger) === true;`
+ * 
+ * > **Restrictions:**
+ * > - Foreign libraries and files are disallowed.
+ * > - Using `Array.prototype.sort()` is disallowed.
+ * @param input {number[]} Array to be sorted
+ * @returns {number[]} Sorted array of numbers
+ */
+function sort(input) {
+    // Your code goes here.
+    return input;
+}
